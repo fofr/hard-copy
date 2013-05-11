479265443b5343a8353eb424079ec6e4765e91bb
Paul <github@prhayes.com>
2013-05-11 11:39:57 +0100

    Add a simple view
    * Setup hogan express as the view engine
    * Create an index view
    * Try out the index view

--- models/app.js
+++ models/app.js
@@ -1,14 +1,19 @@
-define(['express'], function (express) {
+define(['express', 'hogan-express'], function (express, hoganExpress) {
 
     var app = express(),
         port = process.env.PORT || 5000;
+
+    // Setup copy-and-pasta'd from https://github.com/vol4ok/hogan-express
+    app.set('view engine', 'html');
+    //app.enable('view cache');
+    app.engine('html', hoganExpress);
 
     app.configure(function() {
         app.use(express.logger({ format: ':method :url :status' }));
     });
 
     app.get('/', function(request, response) {
-        response.send('Hello RequireJS World!');
+        response.render('index', {test: "TEST"});
     });
 
     app.listen(port, function() {
